3b7190112d83456d1ce10ea90e2243480b5e4738
Muhammad Yusuf Manshur <ymanshur@gmail.com>
2022-11-18 19:27:18 +0700

    Time: 6 ms (32.35%), Space: 2.6 MB (79.50%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.go
+++ 0206-reverse-linked-list/0206-reverse-linked-list.go
@@ -10,30 +10,24 @@ func reverseList(head *ListNode) *ListNode {
     if head == nil {
         return head
     }
-    stack := make([]int, 0)
-    for head != nil {
-        stack = append(stack, head.Val)
-        head = head.Next
-    }
-    topIndex := len(stack)-1
-    // init newHead with top off stack
-    newHead := &ListNode{Val:stack[topIndex]}
-    for i := topIndex-1; i >= 0; i-- {
-        newNode := &ListNode{
-            Val: stack[i],
-        }
-        // get tail pointer, then link to newNode
-        tail := newHead
-        for tail.Next != nil {
-            tail = tail.Next
-        }
-        tail.Next = newNode
-    }
-    return newHead
-}
-
-/**
- * 1. Store the s-linked link into stack
- * 2. Create new s-linked link from the stack
- */
+    var prev *ListNode // point to new head
+    var curr = head // point to head
+    for curr != nil {
+        // saving the next point
+        next := curr.Next
+        // changing it previous
+        curr.Next = prev
+        // setting previous to current
+        prev = curr
+        // setting next to current
+        curr = next
  
+        // list := prev
+        // for list != nil {
+        //     fmt.Printf("%+v->", list.Val)
+        //     list = list.Next
+        // }
+        // fmt.Println()
+    }
+    return prev
+}
