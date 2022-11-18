e38ad5e39d9006165d276ef6495fafeaeead466c
Muhammad Yusuf Manshur <ymanshur@gmail.com>
2022-11-18 07:42:47 +0700

    Time: 118 ms (97.32%), Space: 8 MB (90.92%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.go
@@ -0,0 +1,14 @@
+func maxProfit(prices []int) int {
+    minPrice := prices[0]
+    maxProfit := 0
+    for i := 1; i < len(prices); i++ {
+        if prices[i] < minPrice {
+            minPrice = prices[i]
+        }
+        profit := prices[i] - minPrice
+        if profit > maxProfit {
+            maxProfit = profit
+        }
+    }
+    return maxProfit
+}
