08690e94a257ff7829bec11db59f04050b4ee7e0
Muhammad Yusuf Manshur <ymanshur@gmail.com>
2022-11-19 06:55:49 +0700

    Time: 19 ms (32.21%), Space: 4.4 MB (31.54%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.go
+++ 0141-linked-list-cycle/0141-linked-list-cycle.go
@@ -6,14 +6,23 @@
  * }
  */
 func hasCycle(head *ListNode) bool {
-    nodeMap := make(map[*ListNode]bool)
+    var headNext = head
     for head != nil {
-        if nodeMap[head] {
-            return true
-        } else {
-            nodeMap[head] = true
+        if headNext != nil && headNext.Next != nil {
+            headNext = headNext.Next.Next
+            if head == headNext {
+                return true
+            }
         }
         head = head.Next
     }
     return false
 }
+
+/**
+ * The O(n) time and space complexity is using map or set to store the visited node,
+ * and check if the next node was visited, return true, otheriwise, return false (at the end).
+ * 
+ * But there is O(n) time and O(1) space complexity, I am going to use two pointer with diff pace,
+ * so if the linked-list is cyclic, the pointers will meet.
+ */
