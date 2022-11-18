49b472c570bc20f7dd5f344da8c920f9f4588475
Muhammad Yusuf Manshur <ymanshur@gmail.com>
2022-11-18 15:47:02 +0700

    Time: 2 ms (45.86%), Space: 1.9 MB (87.08%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.go
+++ 0020-valid-parentheses/0020-valid-parentheses.go
@@ -1,6 +1,6 @@
-func isOpenBracket(c string) bool {
-    for _, openBracket := range []string{"(", "{", "["} {
-        if c == openBracket {
+func isOpenBracket(char rune) bool {
+    for _, openBracket := range []rune{'(', '{', '['} {
+        if char == openBracket {
             return true
         }
     }
@@ -8,11 +8,16 @@ func isOpenBracket(c string) bool {
 }
 
 func isValid(s string) bool {
-    var stack string
+    var stack []rune
+    var closeBracket = map[rune]rune{
+        '(' : ')',
+        '{' : '}',
+        '[' : ']',
+    }
     for _, char := range s {
         // if the char is open bracket, push into stack
-        if isOpenBracket(string(char)) {
-            stack += string(char)
+        if isOpenBracket(char) {
+            stack = append(stack, char)
         } else {
             // if the char is close bracket and the stack is empty, return false
             if len(stack) == 0 {
@@ -20,22 +25,13 @@ func isValid(s string) bool {
             }
             // assume the stack is not empty,
             // if the char is not close bracket of top the stack, return false, otherwise, pop from stack 
-            topStack := string(stack[len(stack)-1])
-            switch string(char) {
-            case ")":
-                if  topStack != "(" {
+            lastIndex := len(stack)-1
+            topStack := stack[lastIndex]
+            if  char != closeBracket[topStack] {
                     return false
-                }
-            case "}":
-                if  topStack != "{" {
-                    return false
-                }
-            case "]":
-                if  topStack != "[" {
-                    return false
-                }
+            } else {                
+                stack = stack[:len(stack)-1]            
             }
-            stack = stack[:len(stack)-1]            
         }
     }
     if len(stack) > 0 {
