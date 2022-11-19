dcc174744f768ab6f0efe679ae8b33c988187be9
Muhammad Yusuf Manshur <ymanshur@gmail.com>
2022-11-20 04:54:32 +0700

    Time: 0 ms (100.00%), Space: 2.1 MB (6.07%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.go
@@ -0,0 +1,20 @@
+var numStep func(num int) int
+
+func climbStairs(n int) int {
+    numStepMap := make(map[int]int)
+
+    numStep = func(num int) int {
+        fmt.Println(num)
+        if _, ok := numStepMap[num]; ok {
+            return numStepMap[num]
+        }
+        
+        numStepMap[num] = numStep(num-1) + numStep(num-2)
+        
+        return numStepMap[num]
+    }
+    
+    numStepMap[1], numStepMap[2], numStepMap[3] = 1, 2, 3
+    return numStep(n)
+    
+}
