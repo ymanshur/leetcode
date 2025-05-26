c40b3bab13b0526ba8c9589553126cba17ba4f79
Muhammad Yusuf Manshur <ymanshur@gmail.com>
2025-05-26 17:14:04 +0700

    Time: 0 ms (100%), Space: 65 MB (94.67%) - LeetHub

--- 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.cpp
+++ 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
     int numSubarrayProductLessThanK(vector<int>& nums, int k) {
-        int ans = 0;
-
         if (k <= 1) {
-            return ans;
+            return 0;
         }
 
-        for (int i = 0; i < nums.size(); i++) {
-            int curr = 1;
-            for (int j = i; j >= 0; j--) {
-                curr *= nums[j];
+        int curr = 1;
+        int left = 0;
+        int ans = 0;
 
-                if (curr >= k) {
-                    break;
-                }
+        for (int right = 0; right < nums.size(); right++) {
+            curr *= nums[right];
 
-                ans++;
+            while (curr >= k) {
+                curr /= nums[left];
+                left++;
             }
+
+            ans += (right - left + 1);
         }
 
         return ans;
