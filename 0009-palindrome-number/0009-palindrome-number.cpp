8ce768947f73d86e7f9dece03efd3cd71768664f
Yusuf Manshur <ymanshur@gmail.com>
2025-06-21 21:41:51 +0700

    Time: 0 ms (100%), Space: 8.5 MB (68.6%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        }
+
+        long long y = 0;
+        int temp = x;
+        while (temp > 0) {
+            y = y * 10 + temp % 10;
+            temp /= 10 ;
+        }
+
+        return x == y;
+    }
+};
