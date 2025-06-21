9c4159f6b32a29991fa303ed056d2d5c36c67a1b
Yusuf Manshur <ymanshur@gmail.com>
2025-06-21 13:24:37 +0700

    Time: 10 ms (0.55%), Space: 5.2 MB (74.86%) - LeetHub

---
+++ 0108-convert-sorted-array-to-binary-search-tree/0108-convert-sorted-array-to-binary-search-tree.go
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func sortedArrayToBST(nums []int) *TreeNode {
+    root := BST(nums, 0, len(nums) - 1)
+    print(root)
+    return root
+}
+
+func BST(nums []int, start, end int) *TreeNode {
+    if start > end || end < 0 || start > len(nums) - 1 {
+        return nil
+    }
+
+    mid := (end - start) / 2 + start
+
+    //fmt.Printf("%d %d %d\n", start, mid, end)
+
+    return &TreeNode{
+        Val: nums[mid],
+        Left: BST(nums, start, mid - 1),
+        Right: BST(nums, mid + 1, end),
+    }
+}
+
+func print(root *TreeNode) {
+    if root == nil {
+        fmt.Printf("null ")
+        return
+    }
+
+    //fmt.Printf("%d ", root.Val)
+
+    print(root.Left)
+    print(root.Right)
+}
