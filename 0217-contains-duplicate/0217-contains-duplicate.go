09d73578ef8bca98a266a5cdf88cd1ff72952f9d
M. Yusuf Manshur <ymanshur@gmail.com>
2025-10-26 05:45:19 +0700

    Time: 15 ms (90.93%), Space: 13.2 MB (30.4%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.go
+++ 0217-contains-duplicate/0217-contains-duplicate.go
@@ -1,10 +1,10 @@
 func containsDuplicate(nums []int) bool {
-    numMap := map[int]bool{}
+    unique := make(map[int]int, len(nums))
     for _, num := range nums {
-        if numMap[num] {
+        if _, ok := unique[num]; ok {
             return true
         }
-        numMap[num] = true
+        unique[num] = 1
     }
     return false
 }
