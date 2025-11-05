fe5e4857f7ff62ce6b88560a80ab3b51e11b8e90
M. Yusuf Manshur <ymanshur@gmail.com>
2025-11-05 09:59:17 +0700

    Time: 0 ms (100%), Space: 9.4 MB (12.56%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.go
+++ 0283-move-zeroes/0283-move-zeroes.go
@@ -1,12 +1,11 @@
 func moveZeroes(nums []int)  {
-    l, r := -1, 0
+    l, r := 0, 0
     for r < len(nums) {
-        if l == -1 && nums[r] == 0 {
-            l = r
+        if nums[l] == 0 && nums[r] != 0 {
+            nums[l], nums[r] = nums[r], nums[l]
         }
 
-        if l != -1 && nums[r] != 0 {
-            nums[l], nums[r] = nums[r], nums[l]
+        if nums[l] != 0 {
             l++
         }
 
